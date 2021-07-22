e737ee54daf58bdffcb40f786cba615762a9acea
yungsem <yungsem@126.com>
2021-07-23 00:13:12 +0800

    feat: init

--- base-auth/src/main/java/com/yungsem/auth/service/endpointconfig/BaseTokenEnhancer.java
+++ base-auth/src/main/java/com/yungsem/auth/service/endpointconfig/BaseTokenEnhancer.java
@@ -1,5 +1,6 @@
 package com.yungsem.auth.service.endpointconfig;
 
+import com.yungsem.auth.service.AuthUser;
 import com.yunsem.common.model.bo.LoginUser;
 import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
@@ -18,7 +19,7 @@ public class BaseTokenEnhancer implements TokenEnhancer {
     @Override
     public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
         final Map<String, Object> additionalInfo = new HashMap<>(4);
-        LoginUser authUser = (LoginUser) authentication.getUserAuthentication().getPrincipal();
+        AuthUser authUser = (AuthUser) authentication.getUserAuthentication().getPrincipal();
         additionalInfo.put("id", authUser.getId());
         additionalInfo.put("code", authUser.getCode());
         additionalInfo.put("username", authUser.getUsername());
--- base-mdm/base-mdm-biz/src/main/java/com/yungsem/mdm/BaseMdmApplication.java
+++ base-mdm/base-mdm-biz/src/main/java/com/yungsem/mdm/BaseMdmApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @MapperScan("com.yungsem.**.mapper")
+@ComponentScan(basePackages = {"com.yungsem"})
 @EnableResourceServer // 开启资源保护
 @EnableCaching
 @SpringBootApplication
